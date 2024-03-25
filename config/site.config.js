68f2f6a50b3e47b5759cb31faa88e0733b63e849
MD YUSUF <88443883+cybrous@users.noreply.github.com>
2024-03-25 22:16:36 +0530

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "Cybrous OneDrive",
+  title: "Cybrous Book",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
   baseDirectory: '/GNDU CSE BOOKS & NOTES',
